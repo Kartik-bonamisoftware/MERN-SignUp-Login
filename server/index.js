68459608570c5217ade3ddd488bda6c67f06e758
Kartik-bonamisoftware <kartik.negi@bonamisoftware.com>
2022-11-19 13:56:15 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/mern", () => {
+  console.log("mongodb connect");
+});
+
+app.post("/api/register", async (req, res) => {
+  console.log(req.body);
+  try {
+    const newPassword = await bcrypt.hash(req.body.password, 10);
+    await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: newPassword,
+    });
+    res.json({ status: "ok" });
+  } catch (err) {
+    console.log(err);
+    res.json({ status: "error", error: "Duplicate email" });
+  }
+});
+
+app.post("/api/login", async (req, res) => {
+  console.log(req.body);
+  const user = await User.findOne({
+    email: req.body.email,
+    // password: req.body.password,
+  });
+
+  if (!user) {
+    return { status: "error", error: "Invalid login" };
+  }
+
+  const isPasswordValid = await bcrypt.compare(
+    req.body.password,
+    user.password
+  );
+
+  if (isPasswordValid) {
+    const token = jwt.sign({ name: user.name, email: user.email }, "secret123");
+    return res.json({ status: "ok", user: token });
+  } else {
+    return res.json({ status: "error", user: false });
+  }
+});
+
+app.get("/api/quote", async (req, res) => {
+  const token = req.headers["x-access-token"];
+  try {
+    const decoded = jwt.verify(token, "secret123");
+    const email = decoded.email;
+    const user = await User.findOne({ email: email });
+    return { status: "ok", qoute: user.qoute };
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", error: "Invalid token" });
+  }
+});
+
+app.post("/api/quote", async (req, res) => {
+  const token = req.headers["x-access-token"];
+  try {
+    const decoded = jwt.verify(token, "secret123");
+    const email = decoded.email;
+    await User.updateOne({ email: email }, { $set: { qoute: req.body.qoute } });
+    return res.json({ status: "ok" });
+  } catch (error) {
+    res.json({ status: "error", error: "Invalid token" });
+  }
+});
+
+app.listen(9010, () => {
+  console.log("Server started");
+});
